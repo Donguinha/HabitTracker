f14bf37cd0b3ca693657cb44a4c58f4dfbfafa05
Guilherme Rodrigues <oliveirack1@gmail.com>
2024-10-24 00:04:30 -0300

    add hangfire

--- HabitTracker/App/Program.cs
+++ HabitTracker/App/Program.cs
@@ -1,5 +1,7 @@
 using App.Data;
 using App.Routes;
+using Hangfire;
+using Hangfire.Storage.SQLite;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,12 +10,22 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<DataContext>();
 
+GlobalConfiguration.Configuration.UseSQLiteStorage();
+
+builder.Services.AddHangfire(configuration => configuration
+    .UseSimpleAssemblyNameTypeSerializer()
+    .UseRecommendedSerializerSettings()
+    .UseSQLiteStorage());
+
+builder.Services.AddHangfireServer();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseHangfireDashboard();
 }
 
 app.GoalRoutes();
